034cb7e53b43872005025e21c077f42a94fde4df
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-27 20:49:25 +0530

    completed outer card

--- src/components/NftCardPreview.jsx
+++ src/components/NftCardPreview.jsx
@@ -2,20 +2,22 @@ import React from "react";
 
 const NftCardPreview = () => {
   return (
-    <div className="">
+    <div className="bg-fmLigntBlue rounded-2xl p-6">
       {/* image section  */}
       <div className="w-full overflow-hidden rounded-lg">
         <img src="/image-equilibrium.jpg" alt="image" />
       </div>
       {/* header and para section  */}
       <div className="">
-        <h1 className=""></h1>
-        <p className=""></p>
+        <h1 className="">Equilibrium #3429</h1>
+        <p className="">
+          Our Equilibrium collection promotes balance and calm.
+        </p>
       </div>
       {/* value and time section  */}
       <div className="">
-        <p className=""></p>
-        <p className=""></p>
+        <p className="">0.041 ETH</p>
+        <p className="">3 days left</p>
       </div>
       {/*  Divider  */}
       <div className="" />
