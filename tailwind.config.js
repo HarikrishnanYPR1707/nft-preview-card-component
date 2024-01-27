e2defd4e746605c8b57920f468ded7434b71516a
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-27 20:36:54 +0530

    Added styles from frontend mentor style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fmDarkBlue: "#0d1a2d",
+        fmLigntBlue: "15273f",
+        fmCyan: "#00fff7",
+      },
+      fontFamily: {
+        specimen: ["Outfit", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
